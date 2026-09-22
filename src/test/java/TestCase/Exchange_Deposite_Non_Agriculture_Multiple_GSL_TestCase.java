5097a89e8bf8d538bb1aa4cc93d67bbd90364e82
CCRL2026 <abhishekyt@ccrl.co.in>
2026-09-22 14:18:26 +0530

    22-Sep-2026

--- src/test/java/TestCase/Exchange_Deposite_Non_Agriculture_Multiple_GSL_TestCase.java
+++ src/test/java/TestCase/Exchange_Deposite_Non_Agriculture_Multiple_GSL_TestCase.java
@@ -23,16 +23,16 @@ public class Exchange_Deposite_Non_Agriculture_Multiple_GSL_TestCase extends Bas
 			+ " Exchange_Deposite_Non_Agri_GSL" + +System.currentTimeMillis() + ".html", true);
 	ExtentTest test = Report.startTest("ENWR_Generation for Exchange_Non_Agriculture");
 
-	@Test
+//	@Test
 	void Exchange_Deposite_Request_Non_Agriculture_Maker() throws IOException {
 		try {
 			test.log(LogStatus.INFO, "Exchange_Deposite_Non_Agriculturet_Maker start");
 
 			LoginPages Login = new LoginPages(driver, Wait);
 			// Login.PortalLogin("RP-KML", "user_A", "121@test");
-			 Login.PortalLogin("RP-UATTEST", "user1", "121@test");// Lakshuman Sir
+			 //Login.PortalLogin("RP-UATTEST", "user1", "121@test");// Lakshuman Sir
 			// Login.PortalLogin("RP-COUNTRYWIDE", "Bhanu1", "121@test");//Bhanu
-			//Login.PortalLogin("rp-Abhishek", "user51", "121@test"); // Abhishek
+			Login.PortalLogin("rp-Abhishek", "user51", "121@test"); // Abhishek
 
 			Exchange_Deposite_Request_Non_Agriculture_Maker Depo = new Exchange_Deposite_Request_Non_Agriculture_Maker(
 					driver, Wait);
@@ -50,15 +50,15 @@ public class Exchange_Deposite_Non_Agriculture_Multiple_GSL_TestCase extends Bas
 		Report.flush();
 	}
 
-//	 @Test
+	// @Test
 	void RP_Deposite_Request_Checker() throws IOException {
 		try {
 			test.log(LogStatus.INFO, "Exchange_Deposite_Non_Agriculture_Checker is start");
 			RP_Deposite_Request_Checker Dash = new RP_Deposite_Request_Checker(driver, Wait);
 			LoginPages Login = new LoginPages(driver, Wait);
 			//Login.PortalLogin("RP-KML", "user_B", "121@test");
-			 Login.PortalLogin("RP-UATTEST", "user2", "121@test");
-			// Login.PortalLogin("rp-Abhishek", "user52", "121@test");
+			// Login.PortalLogin("RP-UATTEST", "user2", "121@test");
+			 Login.PortalLogin("rp-Abhishek", "user52", "121@test");
 			// Login.PortalLogin("RP-COUNTRYWIDE", "Bhanu2", "121@test");
 			Dash.Exchange_Deposite_Request_Non_Agriculture_Checker();
 			test.log(LogStatus.PASS, "Exchange_Deposite_Non_Agriculture_Checker is successfull");
@@ -71,14 +71,14 @@ public class Exchange_Deposite_Non_Agriculture_Multiple_GSL_TestCase extends Bas
 	}
 
 	// Database
-	// @Test
+	//@Test
 	void DashBoard_WareHouse_Maker() throws IOException {
 		try {
 			test.log(LogStatus.INFO, "Exchange_Non_Agriculture_Physical_Deposit_Request_Maker is start");
 			LoginPages Login = new LoginPages(driver, Wait);
-			//Login.PortalLogin("wsp9999996", "user1", "121@test");
+			Login.PortalLogin("wsp9999996", "user1", "121@Test");
 			// Login.PortalLogin("WSP8888999", "user3", "121@test");
-			 Login.PortalLogin("WSP8888999", "user2", "121@test");
+			// Login.PortalLogin("WSP8888999", "user2", "121@test");
 			//Login.PortalLogin("wsp9999996", "user71", "121@test");
 			// Login.PortalLogin("wsp9996059", "user102", "121@test");
 			// Login.PortalLogin("WSP9997115", "Bhanu", "121@test");
@@ -100,14 +100,14 @@ public class Exchange_Deposite_Non_Agriculture_Multiple_GSL_TestCase extends Bas
 		Report.flush();
 	}
 
-	// @Test
+	//@Test
 	void DashBoard_WareHouse_Checker() throws IOException {
 		try {
 			test.log(LogStatus.INFO, "Exchange_Non_Agriculture_Physical_Deposit_Request_Checker is start");
 			DashBoard_WareHouse_Checker WSP_CH = new DashBoard_WareHouse_Checker(driver, Wait);
 			LoginPages Login = new LoginPages(driver, Wait);
-			//Login.PortalLogin("wsp9999996", "user2", "121@test");
-			 Login.PortalLogin("WSP8888999", "user3", "121@test");
+			Login.PortalLogin("wsp9999996", "user2", "121@test");
+			 //Login.PortalLogin("WSP8888999", "user3", "121@test");
 			// Login.PortalLogin("wsp9999996", "user72", "121@test");
 			// Login.PortalLogin("wsp9997115", "pratap", "121@test");
 			// Login.PortalLogin("wsp9996059", "user101", "121@test");
@@ -128,7 +128,7 @@ public class Exchange_Deposite_Non_Agriculture_Multiple_GSL_TestCase extends Bas
 			test.log(LogStatus.INFO, "Exchange_Non_Agriculture_Deposite_Assayer_Maker is start");
 			Deposite_Assayer_Maker Assayer = new Deposite_Assayer_Maker(driver, Wait);
 			LoginPages Login = new LoginPages(driver, Wait);
-			Login.PortalLogin("Asyr-3338084", "user1", "121@test");
+			Login.PortalLogin("Asyr-3338084", "user1", "121@Test");
 			// Login.PortalLogin("ASYR-2905202", "Bhanu1", "121@best");
 			// Login.PortalLogin("ASYR-8889999", "user1", "121@test"); //Lakshuman
 			// Login.PortalLogin("Assayer01", "user1", "121@test");
@@ -168,10 +168,10 @@ public class Exchange_Deposite_Non_Agriculture_Multiple_GSL_TestCase extends Bas
 			test.log(LogStatus.INFO, "Exchange_Non_Agriculture_Deposit_Confirm_WH_Maker is start");
 			Deposit_Confirm_WH_Maker Confirn_Maker = new Deposit_Confirm_WH_Maker(driver, Wait);
 			LoginPages Login = new LoginPages(driver, Wait);
-			//Login.PortalLogin("wsp9999996", "user1", "121@test");
+			Login.PortalLogin("wsp9999996", "user1", "121@Test");
 			// Login.PortalLogin("WSP8888999", "user2", "121@test");
 			// Login.PortalLogin("WSP8888999", "user2", "121@test");
-			 Login.PortalLogin("wsp9999996", "user71", "Test@123");
+			// Login.PortalLogin("wsp9999996", "user71", "Test@123");
 			// Login.PortalLogin("wsp9997115", "bhanu", "121@test");
 			// Login.PortalLogin("wsp9996059", "user102", "121@test");
 			// Login.PortalLogin("WSP9996059", "user111", "121@test");
@@ -185,16 +185,16 @@ public class Exchange_Deposite_Non_Agriculture_Multiple_GSL_TestCase extends Bas
 		Report.flush();
 	}
 
-	// @Test
+	 @Test
 	void Deposit_Confirm_WH_Checker() throws IOException {
 		try {
 			test.log(LogStatus.INFO, "Exchange_Non_Agriculture_Deposit_Confirm_WH_Checker is start");
 			Deposit_Confirm_WH_Checker Confirm = new Deposit_Confirm_WH_Checker(driver, Wait);
 			LoginPages Login = new LoginPages(driver, Wait);
-			//Login.PortalLogin("wsp9999996", "user2", "121@test");
+			Login.PortalLogin("wsp9999996", "user2", "121@test");
 			//Login.PortalLogin("WSP8888999", "user3", "121@test");
 			// Login.PortalLogin("WSP8888999", "user3", "121@test");
-			 Login.PortalLogin("wsp9999996", "user72", "121@test");
+			// Login.PortalLogin("wsp9999996", "user72", "121@test");
 			// Login.PortalLogin("wsp9997115", "pratap", "121@test");
 			// Login.PortalLogin("wsp9996059", "user101", "121@test");
 			// Login.PortalLogin("WSP9996059", "user112", "121@test");
